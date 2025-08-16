dfd03a5c360e4afdc5561e725712e9dcc48658b7
sanjeevdotnet <sanjeev.net2010@gmail.com>
2025-08-16 17:02:08 +0530

    assignment 1
    ASSIGNMENT 1:
    Module 2: Basic Python Concepts

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a= int(input('Enter the First Number: '))
+b= int(input('Enter the Second Number: '))
+
+c= a+b
+d= a-b
+e= a*b
+f= a/b
+
+print('\nAddition : ', c,
+      '\nSubtraction : ',d,
+      '\nMultiplication : ',e,
+      '\nDivision : ',f)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+a= input('Enter the First name: ')
+b= input('Enter the Last name: ')
+
+print ('Hello,',a,b ,'! welcome to the Python program.')
